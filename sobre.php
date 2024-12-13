735dae73eafb0580d33a19b6e12d71048de49f03
JulioCesarCandidoMarquezin <juliocesarcandidomarquezin@gmail.com>
2024-12-13 00:38:22 -0400

    Criada apresentacão

---
+++ sobre.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Sobre Mim</title>
+</head>
+<body>
+    <header class="bg-dark text-white text-center p-4">
+        <h1>Sobre Mim</h1>
+    </header>
+    <?php include (__DIR__ . "/components/header.php"); ?>
+
+    <main class="container mt-4">
+        <h2>Júlio César Cândido Marquezin</h2>
+        <p>Turma: 3B</p>
+        <p>Curso: Informática</p>
+        <img src="/assets/eu.png" alt="Minha Foto" class="img-fluid rounded-circle" style="max-width: 200px;">
+    </main>
+    <footer class="text-center mt-4">
+        <p>&copy; 2024 Júlio Marquezin</p>
+    </footer>
+</body>
+</html>
