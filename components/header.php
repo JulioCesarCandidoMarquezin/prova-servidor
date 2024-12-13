c903f6b628a67948f0b6e415154b878d8698065e
JulioCesarCandidoMarquezin <juliocesarcandidomarquezin@gmail.com>
2024-12-13 00:40:45 -0400

    Substituido título de atores por uma imagem

--- components/header.php
+++ components/header.php
@@ -1,15 +1,13 @@
 <header>
     <nav class="navbar navbar-expand-sm bg-light sticky-top bg-dark" data-bs-theme="dark">
         <div class="container-fluid">
-            <span class="navbar-text">
-                <a href="#" class="link-underline link-underline-opacity-0">Atores</a>
-            </span>
+            <img src="/assets/banner.png" alt="Banner" class="d-inline-block align-text-top" style="height: 50px;">
 
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="/index.php">Atores</a></li>
                 <li class="nav-item">
-                    <a class="nav-link link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="/portifolio.php">Portifólio</a></li>
+                    <a class="nav-link link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="/sobre.php">Portifólio</a></li>
             </ul>
         </div>
     </nav>
